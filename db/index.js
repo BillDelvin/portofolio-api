f3b26f31d77302157b369c80bbf4d940f4633a3f
billdelvin <billdelvin6@gmail.com>
2021-04-16 09:43:13 +0700

    fixing connection db

--- db/index.js
+++ db/index.js
@@ -1,20 +1,18 @@
 const mongoose = require('mongoose');
 const config = require('../config/dev');
 
-module.exports = {
- dbConnection: async () => {
-  await mongoose.connect(
-   config.DB_URI,
-   {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useFindAndModify: false,
-    useCreateIndex: true,
-   },
-   (err) => {
-    if (err) console.log(err);
-    else console.log('Connected to DB!');
-   },
-  );
- },
+exports.connect = async () => {
+ return await mongoose.connect(
+  config.DB_URI,
+  {
+   useNewUrlParser: true,
+   useUnifiedTopology: true,
+   useFindAndModify: false,
+   useCreateIndex: true,
+  },
+  (err) => {
+   if (err) console.log(err);
+   else console.log('Connected to DB!');
+  },
+ );
 };
