cdbd839332a42c8cdbee8e9474ad3c818aa0b7e4
billdelvin <billdelvin6@gmail.com>
2021-04-21 11:32:00 +0700

    make schema for portofolio model

--- db/models/portofolio-modal.js
+++ db/models/portofolio-modal.js
@@ -2,8 +2,15 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const portofolioSchema = new Schema({
- title: { type: String, required: true },
+ title: { type: String, required: true, maxLength: 128 },
+ company: { type: String, required: true, maxLength: 64 },
+ companyWebsite: { type: String, required: true, maxLength: 64 },
+ location: { type: String, required: true },
+ jobTitle: { type: String, required: true },
  description: { type: String, required: true },
+ startDate: { type: Date, required: true },
+ endDate: { type: Date },
+ createdAt: { type: Date, default: Date.now() },
 });
 
 module.exports = mongoose.model('portofolios', portofolioSchema);
