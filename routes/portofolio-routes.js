f3c39ce41ee5d4388d4fbc66a92991cf22f3e186
billdelvin <billdelvin6@gmail.com>
2021-04-21 11:32:31 +0700

    adding route for getting data by id

--- routes/portofolio-routes.js
+++ routes/portofolio-routes.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const portofolioController = require('../controllers/portofolio-controller');
 
-router.get('/portoflios', portofolioController.getPortofolio);
+router.get('/portofolios', portofolioController.getPortofolio);
+router.get('/portofolios/:id', portofolioController.getPortofolioById);
 
 module.exports = router;
