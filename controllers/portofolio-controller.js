4936bb4523fd7277fcbc23d2213b1ea8e3e4d134
billdelvin <billdelvin6@gmail.com>
2021-04-21 11:31:32 +0700

    creating controller for get data by id

--- controllers/portofolio-controller.js
+++ controllers/portofolio-controller.js
@@ -14,4 +14,17 @@ module.exports = {
     console.log(err);
    });
  },
+ getPortofolioById: async (req, res) => {
+  const { id } = req.params;
+  try {
+   const getPortofolioById = await portofolioModel.findById(id);
+   if (getPortofolioById) {
+    return res.status(200).json(getPortofolioById);
+   } else {
+    next();
+   }
+  } catch (error) {
+   return res.status(400).json('API Error Message!');
+  }
+ },
 };
