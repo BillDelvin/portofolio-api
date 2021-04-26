19f1fa84f3e8360d10bde3daa3881715d8a35ce3
billdelvin <billdelvin6@gmail.com>
2021-04-26 10:57:15 +0700

    adding middleware

--- routes/portofolio-routes.js
+++ routes/portofolio-routes.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const portofolioController = require('../controllers/portofolio-controller');
+const { checkJwt } = require('../controllers/auth');
 
 router.get('/portofolios', portofolioController.getPortofolio);
 router.get('/portofolios/:id', portofolioController.getPortofolioById);
-router.post('/portofolios', portofolioController.createPortofolio);
+router.post('/portofolios', checkJwt, portofolioController.createPortofolio);
 
 module.exports = router;
