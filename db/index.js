a5e8cd4deb97d811ac48508bbac90c3c15507f33
billdelvin <billdelvin6@gmail.com>
2021-04-28 19:08:41 +0700

    adding blog modal

--- db/index.js
+++ db/index.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 const config = require('../config/dev');
 
 require('./models/portofolio-modal');
+require('./models/blog-modal');
 
 exports.connect = async () => {
  return await mongoose.connect(
